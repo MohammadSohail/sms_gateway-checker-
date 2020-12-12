8e3d51fa0f9dd152ab97d9694e4e98994231b3e7
ISHRAAK <SohailMohammad>
2020-12-12 17:58:36 +0600

    History page data

--- main/lib/history.dart
+++ main/lib/history.dart
@@ -1,24 +1,50 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-// ignore: camel_case_types
+
+// ignore: camel_case_types, must_be_immutable
 class History extends StatefulWidget {
-  String id;
-  History({this.id});
+   String number;
+   String message;
+   String timer;
+   History(String number, String message, String timer) {this.number = number; this.message = message; this.timer = timer;}
+
   @override
   _HistoryState createState() => _HistoryState();
 }
 
 class _HistoryState extends State<History> {
+  // var list = [
+  //   {'id':"123123","date":"20/08/2016", "status" : "true"},{'id':"123124","date":"26/08/2016","status" : "true"},{'id':"123125","date":"26/08/2016","status" : "false"}
+  // ];
+
+  // _loadData() async {
+  //   SharedPreferences pref = await SharedPreferences.getInstance();
+  //   return pref.getString("number");
+  // }
+  //
+  // _save() async {
+  //   SharedPreferences pref = await SharedPreferences.getInstance();
+  //   setState(() {
+  //     pref.setString("number", widget.number);
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
         home: Scaffold(
       appBar: AppBar(
-        leading: Icon(
-          Icons.menu,
-        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: (){
+              Navigator.pop(context);
+            },
+          ),
+        ],
         iconTheme: IconThemeData(
             color: Colors.black
         ),
@@ -31,13 +57,17 @@ class _HistoryState extends State<History> {
           ),),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+
         children: [
-          RaisedButton(
-            child:Text("Home"),
-            onPressed: (){
-              Navigator.pop(context);
-            },
-          )
+          //for(var item in list )
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.history),
+                title: Text(widget.number),
+                subtitle: Text(widget.message),
+                trailing: Icon(Icons.done)
+              ))
         ],
       ),
         ),
--- main/lib/main.dart
+++ main/lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
@@ -11,27 +12,35 @@ import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(MaterialApp(
+    title: 'MyApp',
+    // MaterialApp contains our top-level Navigator
     initialRoute: '/',
     routes: {
-      '/' : (context) => MyApp(),
-      '/history' : (context) => History(),
-    }
+      '/': (BuildContext context) => MyApp(),
+      '/history': (BuildContext context) => History("", "", ""),
+    },
   ));
 }
 
 class MyApp extends StatefulWidget {
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  String _timeString;
+
+  // @override
+  // void initState(){
+  //   super.initState();
+  // }
   //Create a text controller and use it to retrieve the current value
   //   // of the TextField.
   final num = TextEditingController();
   final msg = TextEditingController();
   final tmr = TextEditingController();
 
+  var currentTime = "";
   var history = new List();
 
  void tmp() {
@@ -49,9 +58,12 @@ class _MyAppState extends State<MyApp> {
 
   Future callService() async
   {
-    String url = "http://alphasms.biz/index.php?app=ws&u=Sohail&h=f6d9aec95bb9c4e46dc7265a22a87b4d&op=pv&to=${num.text}&msg=${msg.text}";
-    //String url1 = "https://jobxprss.com/api/send/app_link/";
-    var currentTime = DateFormat('dd-MM-yyyy – kk:mm').format(DateTime.now());
+    HttpClient client = new HttpClient();
+    client.badCertificateCallback = ((X509Certificate cert, String host, int port) => true);
+
+    //String url = "https://alphasms.biz/index.php?app=ws&u=Sohail&h=f6d9aec95bb9c4e46dc7265a22a87b4d&op=pv&to=${num.text}&msg=${msg.text}";
+    String url1 = "https://jobxprss.com/api/send/app_link/";
+     currentTime = DateFormat('dd-MM-yyyy – kk:mm').format(DateTime.now());
     //print(msg.text + "\n Created at: " + currentTime);
     history.add({
       "number" : num.text,
@@ -59,9 +71,12 @@ class _MyAppState extends State<MyApp> {
       "timer" : tmr.text
     });
     print(history);
+    var tim = int.parse(tmr.text);
     try {
       http.Response res = await http.get(
-          Uri.encodeFull(url),
+          //Timer.periodic(Duration(seconds: tim), (timer) => setState(() {
+            Uri.encodeFull(url1),
+          //})),
           headers: {
             "Accept" : "Application/json"
           }
@@ -78,107 +93,126 @@ class _MyAppState extends State<MyApp> {
     // Clean up the controller when the widget is disposed.
     num.dispose();
     msg.dispose();
+    tmr.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-
-    final history = ModalRoute.of(context).settings.arguments;
-
+    //final history = ModalRoute.of(context).settings.arguments;
     return new Builder(
         builder: (ctx) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('SMS gateway_checker'),
+          title: Text('SMS Gateway_Checker', style: TextStyle(
+            fontSize: 20,
+            color: Colors.black,
+          ),),
+          backgroundColor: Colors.yellow,
         ),
         body: SafeArea(
-          child: Column(
-              children: [
-                TextFormField(
-                  controller: num,
-                  decoration: InputDecoration(
-                      hintText: 'Phone Number',
-                      border: OutlineInputBorder(),
-                      icon: Icon(Icons.phone)
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: num,
+                    decoration: InputDecoration(
+                        hintText: 'Phone Number',
+                        border: OutlineInputBorder(),
+                        icon: Icon(Icons.phone)
+                    ),
                   ),
-                ),
-                TextFormField(
-                  controller: msg,
-                  decoration: InputDecoration(
-                      hintText: 'Message',
-                      border: OutlineInputBorder(),
-                    icon: Icon(Icons.message)
+                  TextFormField(
+                    controller: msg,
+                    decoration: InputDecoration(
+                        hintText: 'Message',
+                        border: OutlineInputBorder(),
+                        icon: Icon(Icons.message)
+                    ),
                   ),
-                ),
-                TextFormField(
-                  controller: tmr,
-                  decoration: InputDecoration(
-                      hintText: 'Timer',
-                      border: OutlineInputBorder(),
-                      icon: Icon(Icons.timer_sharp)
+                  TextFormField(
+                    controller: tmr,
+                    decoration: InputDecoration(
+                        hintText: 'Timer',
+                        border: OutlineInputBorder(),
+                        icon: Icon(Icons.timer_sharp)
+                    ),
                   ),
-                ),
-                RaisedButton(
-                  onPressed: callService,
-                  child: Text('Send Message',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+                  RaisedButton(
+                    onPressed: callService,
+                    child: Text('Send Message',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)
+                    ),
                   ),
-                ),
-                RaisedButton(
-                  child: Column(
-                    children: [
-                      Text("History",style: TextStyle(fontSize: 20),),
-                    ],
+                  RaisedButton(
+                    child: Column(
+                      children: [
+                        Text("History", style: TextStyle(fontSize: 20),),
+                      ],
+                    ),
+                    onPressed: () {
+                      //Route route = MaterialPageRoute(builder: (context) => History());
+                      //Navigator.pushNamed(context, '/history');
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                            new History(num.text, msg.text + '  Created At: ' + currentTime, tmr.text),
+                          ));
+                    },
+                    textColor: Colors.black,
                   ),
-                  onPressed: () {
-                    //Route route = MaterialPageRoute(builder: (context) => History());
-                    Navigator.pushNamed(context, '/history');
-                  },
-                  textColor: Colors.black,
-                ),
-                SizedBox(height: 25.0,),Center(
-                  child: SizedBox(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
+                  SizedBox(height: 25.0,),
+                  Center(
+                    child: SizedBox(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
                           "Current Server Time:",
-                        textAlign: TextAlign.center,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
                       ),
                     ),
                   ),
-                ),
 
-                Center(
-                  child: SizedBox(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
+                  Center(
+                    child: SizedBox(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
                         child: Text(
-                            DateFormat('dd-MM-yyyy – kk:mm').format(DateTime.now()),
+                          DateFormat('dd-MM-yyyy – kk:mm').format(
+                              DateTime.now()),
                           textAlign: TextAlign.center,
                           overflow: TextOverflow.ellipsis,
-                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
                         ),
+                      ),
                     ),
                   ),
-                ),
-                //###############################
-                SizedBox(
-                  height: 200,
-                  child: ListView(
-                    scrollDirection: Axis.vertical,
-                    children: <Widget>[
-                      if(history != null)Text(history,
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
-                    ],
-                  ),
-                ),
-                //#################################
-              ],
+                  //###############################
+                  // SizedBox(
+                  //   height: 200,
+                  //   child: ListView(
+                  //     scrollDirection: Axis.vertical,
+                  //     children: <Widget>[
+                  //       Text("History",
+                  //         style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
+                  //     ],
+                  //   ),
+                  // ),
+                  //#################################
+                ],
+              ),
+            ),
           ),
 
 
